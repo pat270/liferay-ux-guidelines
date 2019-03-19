5e53da164585ffc180be4c84992766ffc47c8949
Patrick Yeo <patrick.yeo@liferay.com>
2019-03-19 09:28:32 -0700

    Add versioning

---
+++ site-js/version-list.js
@@ -0,0 +1,45 @@
+var clayVersions = [
+	'Select Version',
+	'2.3.4',
+	'2.0.3',
+	'2.0.0-rc.11',
+	'2.0.0-rc.10',
+	'2.0.0-rc.4',
+	'2.0.0-rc.3',
+	'2.0.0-rc.2',
+	'2.0.0-rc.1',
+	'2.0.0-rc.0',
+	'2.0.0-beta.8',
+	'2.0.0-beta.6',
+	'2.0.0-beta.5',
+	'2.0.0-beta.4',
+	'2.0.0-beta.3',
+	'2.0.0-beta.2',
+	'2.0.0-beta.1',
+	'2.0.0-alpha.11',
+	'2.0.0-alpha.10',
+	'2.0.0-alpha.9',
+	'2.0.0-alpha.8',
+	'2.0.0-alpha.7',
+	'2.0.0-alpha.6',
+	'2.0.0-alpha.5',
+	'2.0.0-alpha.4',
+];
+
+document.addEventListener('DOMContentLoaded', function(event) {
+	var clayVersionList = document.getElementById('clayVersionList');
+
+	for (var i = 0; i < clayVersions.length; i++) {
+		var optionEl = document.createElement('option');
+		var optionElText = document.createTextNode(clayVersions[i]);
+
+		optionEl.appendChild(optionElText);
+		clayVersionList.appendChild(optionEl);
+	}
+
+	clayVersionList.addEventListener('change', function(event) {
+		var selectedText = this.options[this.options.selectedIndex].text;
+
+		window.location.href = './v' + selectedText;
+	});
+});
